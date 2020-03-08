3d9c694e5cfa4454d774849eacd4dd1f229d67c5
keisei77 <keisei.jia@qq.com>
2020-03-08 21:37:48 +0800

    :beers: add test code

--- pages/api/ncov.js
+++ pages/api/ncov.js
@@ -2,16 +2,17 @@ const puppeteer = require('puppeteer');
 
 const parsePage = async () => {
   try {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-    const page = await browser.newPage();
-    await page.goto('https://ncov.dxy.cn/ncovh5/view/pneumonia_peopleapp');
-    const data = await page.evaluate(() => {
-      return {
-        overseas: getListByCountryTypeService2,
-        homeland: getAreaStat
-      };
-    });
-    await browser.close();
+    const data = { overseas: [] };
+    // const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    // const page = await browser.newPage();
+    // await page.goto('https://ncov.dxy.cn/ncovh5/view/pneumonia_peopleapp');
+    // const data = await page.evaluate(() => {
+    //   return {
+    //     overseas: getListByCountryTypeService2,
+    //     homeland: getAreaStat
+    //   };
+    // });
+    // await browser.close();
     return data;
   } catch (error) {
     throw new Error(error);
