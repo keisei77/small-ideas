e79c60ae9ff4f0074bfc70d41058bd406c1d3873
keisei77 <keisei.jia@qq.com>
2020-03-08 21:14:10 +0800

    :art: add args to run puppeteer

--- pages/api/ncov.js
+++ pages/api/ncov.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 const parsePage = async () => {
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.goto('https://ncov.dxy.cn/ncovh5/view/pneumonia_peopleapp');
     const data = await page.evaluate(() => {
