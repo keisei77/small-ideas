2cfb1aa7071d5edf6aa321fead13a3b6ca7b3d7c
keisei77 <keisei.jia@qq.com>
2020-03-08 14:54:48 +0800

    :ok_hand: refine code

--- pages/api/ncov.js
+++ pages/api/ncov.js
@@ -1,12 +1,11 @@
 const puppeteer = require('puppeteer');
 
 const parsePage = async () => {
-  let data;
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://ncov.dxy.cn/ncovh5/view/pneumonia_peopleapp');
-    data = await page.evaluate(() => {
+    const data = await page.evaluate(() => {
       return {
         overseas: getListByCountryTypeService2,
         homeland: getAreaStat
