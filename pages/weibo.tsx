ee41f50d60bb71f790a86cbd04a9ddb92f26998b
keisei77 <keisei.jia@qq.com>
2020-04-01 23:37:52 +0800

    :star2: update weibo page

--- pages/weibo.tsx
+++ pages/weibo.tsx
@@ -2,17 +2,20 @@ import React, { useState, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 import fetch from 'isomorphic-unfetch';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import { pseudoRandomBytes } from 'crypto';
 
 const Weibo = props => {
   const data = props.data;
-  const [currentPage, setCurrentPage] = useState<number>(1);
   const [fullData, setFullData] = useState(data);
   const [topicsExpand, setTopicsExpand] = useState<boolean[]>(
     Array(data.length).fill(false)
   );
   const [loading, setLoading] = useState<boolean>(false);
 
-  const updateTopicsExpand = useCallback(topicIndex => {
+  const updateTopicsExpand = useCallback(async (expanded, link, topicIndex) => {
+    if (!expanded) {
+      await loadWeiboDetail(link, topicIndex);
+    }
     setTopicsExpand(
       prevExpand => (
         (prevExpand[topicIndex] = !prevExpand[topicIndex]), [...prevExpand]
@@ -20,64 +23,98 @@ const Weibo = props => {
     );
   }, []);
 
-  const loadingMore = useCallback(async () => {
+  const loadWeiboDetail = useCallback(async (link, index) => {
     setLoading(true);
-    const nextPageData = await fetch(
-      `https://micro-backend.herokuapp.com/api/weibo?page=${currentPage + 1}`
+    const weiboDetailData = await fetch(
+      `https://micro-backend.herokuapp.com/api/weibo-detail?link=${link}`
     );
-    const nextData = await nextPageData.json();
+    const weiboDetail = await weiboDetailData.json();
     setLoading(false);
+
     ReactDOM.unstable_batchedUpdates(() => {
-      setFullData(prevData => [...prevData, ...nextData]);
-      setCurrentPage(prevPage => prevPage + 1);
+      setFullData(prevData =>
+        prevData.map((data, idx) => {
+          if (idx === index) {
+            return { ...data, ...weiboDetail };
+          } else {
+            return data;
+          }
+        })
+      );
     });
-  }, [currentPage]);
+  }, []);
 
   return (
-    <InfiniteScroll
-      next={loadingMore}
-      hasMore={fullData.length < 50}
-      loader={loading ? <h3>Loading...</h3> : null}
-      dataLength={fullData.length}
-    >
-      <ol className="px-8">
-        {fullData.map((topic, topicIndex) => (
-          <li className="my-4" key={topic.title}>
-            <div className="text-xl text-indigo-500">{topic.title}</div>
-            {topic.lead ? (
-              <div className="pt-1 text-xs text-gray-500">{topic.lead}</div>
-            ) : null}
-            {topic.feedContent.length ? (
-              topicsExpand[topicIndex] ? (
-                <>
-                  {topic.feedContent.map((feed, index) => (
-                    <p
-                      className="odd:bg-yellow-100 even:bg-white pt-1 text-sm text-purple-900"
-                      key={index}
-                    >
-                      {feed}
-                    </p>
-                  ))}
-                  <a
-                    className="cursor-pointer"
-                    onClick={() => updateTopicsExpand(topicIndex)}
-                  >
-                    收起
-                  </a>
-                </>
-              ) : (
-                <a
-                  className="cursor-pointer"
-                  onClick={() => updateTopicsExpand(topicIndex)}
+    <ol className="px-8">
+      {fullData.map((topic, topicIndex) => (
+        <li className="my-4" key={topic.title}>
+          <div className="text-xl text-indigo-500">{topic.title}</div>
+          {loading ? (
+            <span>加载中...</span>
+          ) : (
+            <>
+              {topic.lead ? (
+                <div className="pt-1 text-xs text-gray-500">{topic.lead}</div>
+              ) : null}
+              {topic.feedContent && topic.feedContent.length ? (
+                topicsExpand[topicIndex] ? (
+                  <>
+                    {topic.feedContent.map((feed, index) => (
+                      <p
+                        className="odd:bg-yellow-100 even:bg-white pt-1 text-sm text-purple-900"
+                        key={index}
+                      >
+                        {feed}
+                      </p>
+                    ))}
+                  </>
+                ) : (
+                  <span>收起</span>
+                )
+              ) : null}
+            </>
+          )}
+
+          {topicsExpand[topicIndex] ? (
+            <>
+              {topic.feedContent.map((feed, index) => (
+                <p
+                  className="odd:bg-yellow-100 even:bg-white pt-1 text-sm text-purple-900"
+                  key={index}
                 >
-                  展开
-                </a>
-              )
-            ) : null}
-          </li>
-        ))}
-      </ol>
-    </InfiniteScroll>
+                  {feed}
+                </p>
+              ))}
+              <a
+                className="cursor-pointer"
+                onClick={() =>
+                  updateTopicsExpand(
+                    topicsExpand[topicIndex],
+                    topic.link,
+                    topicIndex
+                  )
+                }
+              >
+                收起
+              </a>
+            </>
+          ) : (
+            <a
+              className="cursor-pointer"
+              onClick={() =>
+                updateTopicsExpand(
+                  topicsExpand[topicIndex],
+                  topic.link,
+                  topicIndex
+                )
+              }
+            >
+              展开
+            </a>
+          )}
+        </li>
+      ))}
+    </ol>
   );
 };
 
