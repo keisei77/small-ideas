e35a439d55ebbf360bcf4fb2dc55c5fc0bbba8e4
keisei77 <keisei.jia@qq.com>
2020-04-02 20:33:21 +0800

    :fire: remove web push

--- components/Head.tsx
+++ components/Head.tsx
@@ -29,11 +29,6 @@ const Head = props => (
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
     <link rel="manifest" href="/manifest.json" />
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.src = "https://cdn.webpushr.com/app.min.js";fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));webpushr('init','BMapj2m7sWJ7OdknKqr7mR_lKBMHK_IvcEMCl7RSEY4VSpHtP2yKWJAUwvUveH10MsrMiDQfVW1JMjAtLPp6l0g');`
-      }}
-    />
   </NextHead>
 );
 
