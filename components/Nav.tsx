88eacd1c65fa2a3a00316611fc6320438261b742
keisei77 <keisei.jia@qq.com>
2020-08-10 22:31:51 +0800

    :alien: fix nav color due to antd

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -16,17 +16,18 @@ const Nav = (props: NavProps) => {
   const router = useRouter();
   return (
     <nav>
-      <ul className="shadow-md text-lg bg-white fixed flex h-12 top-0 w-full z-10 items-center text-orange-500">
-        {menu.map(item => {
+      <ul className="shadow-md text-lg bg-white fixed flex h-12 top-0 w-full z-10 items-center">
+        {menu.map((item) => {
           return (
-            <li
-              className={classNames('ml-8', {
-                'text-indigo-600': item.link === router.pathname
-              })}
-              key={item.label}
-            >
+            <li className="ml-8" key={item.label}>
               <Link href={item.link}>
-                <a>{item.label}</a>
+                <a
+                  className={classNames('text-orange-500', {
+                    'text-indigo-600': item.link === router.pathname,
+                  })}
+                >
+                  {item.label}
+                </a>
               </Link>
             </li>
           );
